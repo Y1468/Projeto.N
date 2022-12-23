5597c0ee7e76632a2315725c49cf49e11a723f02
Ivan Sousa <120279772+Y1468@users.noreply.github.com>
2022-12-23 09:17:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var email=document.getElementById('email');
+
+email.addEventListener('focus',()=>{
+    email.style.borderColor='#08090f';
+});
+
+email.addEventListener('blur',()=>{
+    email.style.borderColor='#08090f';
+});
+
+
+
+
+var passw=document.getElementById('paswor');
+
+passw.addEventListener('focus',()=>{
+    passw.style.borderColor='#08090f';
+});
+   
+ 
+
+  
+    
+
+    
